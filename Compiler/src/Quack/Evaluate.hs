643f5a161f00ce25cbaaaf5a327e451ffb911ac0
MeiyouMayo <no@no.nom>
2022-12-29 07:25:23 -0600

    inlining vars now takes free variables into account

--- Compiler/src/Quack/Evaluate.hs
+++ Compiler/src/Quack/Evaluate.hs
@@ -4,6 +4,8 @@ module Quack.Evaluate where
 import Quack.Common
 
 import qualified Data.HashMap.Strict as H
+import Data.List
+import Control.Monad.State.Lazy
 
 fixpointFrom :: (Eq a) => (a -> a) -> a -> a
 fixpointFrom f a = 
@@ -12,20 +14,28 @@ fixpointFrom f a =
         then a
         else fixpointFrom f a'
 
-spanMap :: (LCExp -> LCExp) -> LCExp -> LCExp
-spanMap f (LCApp l r) = f $ LCApp (f `spanMap` l) (f `spanMap` r)
-spanMap f (LCLam param (Just body)) = f $ LCLam param (Just $ f `spanMap` body) 
-spanMap f label@(LCLabel _) = f label 
-
-
 inlineVars :: LCExp -> [Definition] -> LCExp
 inlineVars exp lcpEnv = 
     let env = H.fromList lcpEnv
-    in spanMap (varMapper $ env) exp 
-    where   varMapper :: H.HashMap Label LCExp -> LCExp -> LCExp
-            varMapper env orig@(LCLabel l) = H.lookupDefault orig l env 
-            varMapper env other = other
+    in inlineVars' [] env exp 
 
+inlineVars' :: [Label] -> H.HashMap Label LCExp -> LCExp -> LCExp
+inlineVars' labels env = inlineVars'' labels env
+    where   inliner inlineLabels body = inlineVars'' inlineLabels env body 
+            inlineVars'' :: [Label] -> H.HashMap Label LCExp -> LCExp -> LCExp
+
+            inlineVars'' labels env exp@(LCLabel l) 
+                | l `elem` labels = exp
+                | otherwise         = H.lookupDefault exp l env
+
+            inlineVars'' labels env exp@(LCApp l r) = 
+                LCApp (inliner labels l) (inliner labels r)
+
+            inlineVars'' labels env exp@(LCLam param (Just body))  =
+                let labels' = (param:labels)
+                in LCLam param $ Just $ inliner labels' body 
+
+-- do something similar here as inlining: keep track of [Label] and (freshVar :: Int) and if lambda param in [Label] param=freshVar freshVar ++
 evaluate :: LCProgram -> LCExp
 evaluate LCProgram{lcpExp=exp, lcpEnv} =
     let exp' = inlineVars exp lcpEnv 
